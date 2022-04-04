5fa28a43cbb3fb378cd5f2dddeb57aa99905a9ee
Mariusz <czerkiesmariusz@gmail.com>
2022-04-04 19:38:39 +0200

    first commit

---
+++ src/main/java/com/example/ageofempire2game/AgeOfEmpire2GameApplication.java
@@ -0,0 +1,13 @@
+package com.example.ageofempire2game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeOfEmpire2GameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgeOfEmpire2GameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ageofempire2game/AgeOfEmpire2GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ageofempire2game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeOfEmpire2GameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
